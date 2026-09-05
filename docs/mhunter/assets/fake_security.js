4360bc56e8e07348bc48ad32680159f27d05fc6e
Your Name <ackmanx@users.noreply.github.com>
2026-09-05 17:03:15 -0500

    mhunter

--- docs/mhunter/assets/fake_security.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('button')?.addEventListener('click', ()=>{
-    const password = document.querySelector('input')?.value;
-    if (!password) {
-        throw new Error('The impossible happened. Password input not found');
-    }
-    localStorage.setItem('super_secret', password);
-    window.location.href = 'index.html';
-});
